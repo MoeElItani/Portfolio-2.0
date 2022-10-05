61b6c61cb376993b063e41a6aebf201f53a4447e
MoeElItani <mhmd98itani@gmail.com>
2022-10-05 16:24:37 +0300

    Technologies Section

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,15 @@ import {
 // Images
 import Image from 'next/image'
 import me from '../public/mohammad-circular 523x523.png'
+import design from '../public/design.png'
+import code from '../public/code.png'
+import consulting from '../public/consulting.png'
 
 export default function Home() {
+   const oddLi =
+      'odd:text-emerald-400 text-gray-600 font-medium'
+   const evenLi =
+      'even:text-emerald-400 text-gray-600 font-medium'
    return (
       <div>
          <Head>
@@ -24,9 +31,11 @@ export default function Home() {
             />
             <link rel='icon' href='/favicon.ico' />
          </Head>
-         <main className='bg-white px-10'>
+
+         <main className='bg-white px-5 md:px-20 lg:px-40'>
+            {/* Front Page */}
             <section className='min-h-screen'>
-               <nav className='py-10 mb-12 flex justify-between'>
+               <nav className='py-10 mb-10 flex justify-between items-center mx-auto md:mb-4'>
                   <h1 className='font-burtons text-xl'>
                      Mohammad El Itani
                   </h1>
@@ -44,14 +53,14 @@ export default function Home() {
                      </li>
                   </ul>
                </nav>
-               <div className='text-center p-2 flex justify-center items-center flex-col'>
-                  <h2 className='text-5xl py-2 text-emerald-400 font-medium'>
+               <div className='text-center flex justify-center items-center flex-col'>
+                  <h2 className='text-5xl text-emerald-400 font-medium md:text-6xl'>
                      Mohammad El Itani
                   </h2>
-                  <h3 className='text-2xl py-2'>
+                  <h3 className='text-2xl py-2 md:text-3xl'>
                      Junior React Front-End Developer
                   </h3>
-                  <p className='text-md leading-8 text-gray-800'>
+                  <p className='text-md py-2 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto'>
                      A fresh Computer Science Graduate
                      that&apos;s passionate about Web
                      Development and its latest and most
@@ -64,25 +73,95 @@ export default function Home() {
                      target='_blank'
                      rel='noreferrer'
                   >
-                     <AiFillGithub />
+                     <AiFillGithub className='hover:rotate-12' />
                   </a>
                   <a
                      href='https://www.linkedin.com/in/mohammad-el-itani/'
                      target='_blank'
                      rel='noreferrer'
                   >
-                     <AiFillLinkedin />
+                     <AiFillLinkedin className='hover:-rotate-12' />
                   </a>
                   <a
                      href='https://www.instagram.com/_mhmditani/'
                      target='_blank'
                      rel='noreferrer'
                   >
-                     <AiFillInstagram />
+                     <AiFillInstagram className='hover:rotate-12' />
                   </a>
                </div>
-               <div className='relative mt-12 flex justify-center items-center mx-auto w-80 h-80'>
+               <div className='relative mt-12 flex justify-center items-center mx-auto w-80 h-80 md:h-96 md:w-96 md:mt-6'>
                   <Image src={me} alt='Mohammad El Itani' />
+               </div>
+
+               {/* Second Page */}
+               <div className='text-center mx-auto'>
+                  <h3 className='text-3xl font-medium py-1 mt-20 mx-auto'>
+                     Front-End Technologies
+                  </h3>
+                  <p className='text-md py-4 leading-8 text-gray-800 max-w-xl mx-auto'>
+                     I usually look for the newest and most
+                     demanded
+                     <span className='text-emerald-400 inline'>
+                        {' '}
+                        technologies
+                     </span>{' '}
+                     to learn and improve my skill set
+                  </p>
+                  <p className='text-md py-1 leading-8 text-gray-800 max-w-xl mx-auto'>
+                     So currently, I use various
+                     technologies that include but are not
+                     limited to:
+                  </p>
+                  <div className='hover:scale-110 inline-block shadow-lg text-center p-10 rounded-xl my-6'>
+                     <Image
+                        width={100}
+                        height={100}
+                        src={code}
+                        alt='code'
+                     />
+                     <ul>
+                        <li className={oddLi}>HTML</li>
+                        <li className={oddLi}>CSS</li>
+                        <li className={oddLi}>
+                           JavaScript
+                        </li>
+                     </ul>
+                  </div>
+                  <div className='hover:scale-110  inline-block shadow-lg text-center p-10 rounded-xl m-6'>
+                     <Image
+                        width={100}
+                        height={100}
+                        src={consulting}
+                        alt='consulting'
+                     />
+                     <ul>
+                        <li className={evenLi}>
+                           React/Next
+                        </li>
+                        <li className={evenLi}>
+                           Redux Toolkit
+                        </li>
+                        <li className={evenLi}>
+                           SASS/SCSS
+                        </li>
+                     </ul>
+                  </div>
+                  <div className='hover:scale-110 inline-block shadow-lg text-center p-10 rounded-xl my-6'>
+                     <Image
+                        width={100}
+                        height={100}
+                        src={design}
+                        alt='design'
+                     />
+                     <ul>
+                        <li className={oddLi}>Tailwind</li>
+                        <li className={oddLi}>API</li>
+                        <li className={oddLi}>
+                           Git/Github
+                        </li>
+                     </ul>
+                  </div>
                </div>
             </section>
          </main>
