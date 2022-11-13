68cff28482a1d66a8678e7967db3dda4b438e203
MoeElItani <mhmd98itani@gmail.com>
2022-11-13 07:28:54 +0200

    updated projects and made them clickable links

--- pages/components/Projects.jsx
+++ pages/components/Projects.jsx
@@ -32,59 +32,95 @@ const Projects = () => {
             </p>
          </div>
          <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
-            <div className='hover:scale-110 shadow-lg basis-1/3 flex-1 rounded-xl'>
-               <h2>Clothing E-commerce</h2>
-               <Image
-                  className='rounded-xl'
-                  layout='responsive'
-                  src={web1}
-                  alt='Project 1'
-               />
+            <div className='hover:scale-110 hover:cursor-pointer shadow-lg basis-1/3 flex-1 rounded-xl'>
+               <a
+                  href='https://bank-app-beta.vercel.app/'
+                  target='_blank'
+                  rel='noreferrer'
+               >
+                  <h2>Bank App</h2>
+                  <Image
+                     className='rounded-xl'
+                     layout='responsive'
+                     src={web1}
+                     alt='Project 1'
+                  />
+               </a>
             </div>
             <div className='hover:scale-110 shadow-lg basis-1/3 flex-1 rounded-xl'>
-               <h2>Videography Portfolio</h2>
-               <Image
-                  className='rounded-xl'
-                  layout='responsive'
-                  src={web2}
-                  alt='Project 2'
-               />
+               <a
+                  href='https://capture-videography.vercel.app'
+                  target='_blank'
+                  rel='noreferrer'
+               >
+                  <h2>Videography Portfolio</h2>
+                  <Image
+                     className='rounded-xl'
+                     layout='responsive'
+                     src={web2}
+                     alt='Project 2'
+                  />
+               </a>
             </div>
             <div className='hover:scale-110 shadow-lg basis-1/3 flex-1 rounded-xl'>
-               <h2>Candy Shop</h2>
-               <Image
-                  className='rounded-xl'
-                  layout='responsive'
-                  src={web3}
-                  alt='Project 3'
-               />
+               <h2>Clothing Ecommerce</h2>
+               <a
+                  href='https://react-clothing-ecommerce.vercel.app/'
+                  target='_blank'
+                  rel='noreferrer'
+               >
+                  <Image
+                     className='rounded-xl'
+                     layout='responsive'
+                     src={web3}
+                     alt='Project 3'
+                  />
+               </a>
             </div>
             <div className='hover:scale-110  basis-1/3 flex-1 shadow-lg rounded-xl'>
-               <h2>Music Player</h2>
-               <Image
-                  className='rounded-xl'
-                  layout='responsive'
-                  src={web4}
-                  alt='Project 4'
-               />
+               <a
+                  href='https://click-n-eat.vercel.app/'
+                  target='_blank'
+                  rel='noreferrer'
+               >
+                  <h2>Food Ordering System</h2>
+                  <Image
+                     className='rounded-xl'
+                     layout='responsive'
+                     src={web4}
+                     alt='Project 4'
+                  />
+               </a>
             </div>
             <div className='hover:scale-110 shadow-lg basis-1/3 flex-1 rounded-xl'>
-               <h2>Youtube 2.0</h2>
-               <Image
-                  className='rounded-lg'
-                  layout='responsive'
-                  src={web5}
-                  alt='Project 5'
-               />
+               <a
+                  href='https://youtube-2-0-2hb93edkg-moeelitani.vercel.app'
+                  target='_blank'
+                  rel='noreferrer'
+               >
+                  <h2>Youtube 2.0</h2>
+                  <Image
+                     className='rounded-lg'
+                     layout='responsive'
+                     src={web5}
+                     alt='Project 5'
+                  />
+               </a>
             </div>
             <div className='hover:scale-110 shadow-lg basis-1/3 flex-1 rounded-xl'>
-               <h2>Food Ordering System</h2>
-               <Image
-                  className='rounded-lg'
-                  layout='responsive'
-                  src={web6}
-                  alt='Project 6'
-               />
+               <a
+                  href='https://waves-music-player-orcin.vercel.app'
+                  target='_blank'
+                  rel='noreferrer'
+               >
+                  <h2>Music Player</h2>
+                  <Image
+                     className='rounded-lg'
+                     layout='responsive'
+                     src={web6}
+                     alt='Project 6'
+                  />
+               </a>
             </div>
          </div>
       </section>
